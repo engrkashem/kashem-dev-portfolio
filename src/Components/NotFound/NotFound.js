e505b4fa7566b26556dc486554f01aeb8a36e38b
engrkashem <kashemaust@gmail.com>
2022-06-10 20:49:10 +0600

    notfound page added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Page Not Found</h2>
+        <div className='not-found bg-cover min-h-screen'>
+            <h2 className=' text-6xl font-bold text-blue-800 text-center pt-5'>The Page Not Found</h2> <Link className=' block text-center text-2xl mt-2 font-semibold text-blue-700' to={'/'}>Back to Home</Link>
         </div>
     );
 };
