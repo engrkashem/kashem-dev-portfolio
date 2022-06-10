f6f7abc9e9df8210909a3009369450c76f75844a
engrkashem <kashemaust@gmail.com>
2022-06-10 12:57:16 +0600

    all projects are showing in projects page from fake data

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import useProjects from '../../hooks/useProjects';
+import Project from '../Shared/Project';
 import SideNav from '../SideNav/SideNav';
 const Projects = () => {
-
+    //load all projects
+    const [projects] = useProjects();
     return (
         <div className='bg-slate-900 min-h-screen p-5'>
             <SideNav></SideNav>
             <div className='container flex flex-col gap-10'>
                 <h2 className=' text-center font-bold text-6xl text-yellow-400 py-1 uppercase'>All Projects</h2>
-
+                {
+                    projects.map(project => <Project
+                        key={project.id}
+                        project={project}
+                    ></Project>)
+                }
 
             </div>
         </div>
